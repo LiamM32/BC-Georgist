176635d4bb71a7b491f21f1f825f0a158e69cac3
electric-gecko <liammcgillivray32@gmail.com>
2023-10-01 23:54:58 -0700

    Changed absolute to relative path in insert-html.js.

--- insert-html.js
+++ insert-html.js
@@ -1,7 +1,7 @@
 // Function to load and insert header content
 function loadHeader() {
 	// Use jQuery's load() function to fetch the header.html content
-	$('#header-container').load('/header.html');
+	$('#header-container').load('header.html');
 }
 
 // Call the loadHeader() function when the document is ready
